f544ebc2c45aec10d3b4322bc883e2a81d78a45a
Sharmin <sharmin.idb.info@gmail.com>
2024-02-14 11:52:18 +0600

    3rd part

--- admin/class/function.php
+++ admin/class/function.php
@@ -16,10 +16,40 @@
             {
                 die("Database Connection Error!!");
             }
+        }
+
+        public function admin_login($data){
+            $admin_email = $data['admin_email'];
+            $admin_pass = md5($data['admin_pass']);
+
+            $query = "SELECT * FROM admin_info WHERE admin_email='$admin_email' && admin_pass='$admin_pass'";
+
+            if(mysqli_query($this->conn, $query)){
+                $admin_info = mysqli_query($this->conn, $query);
+
+                if($admin_info){
+                header("location:dashboard.php");
+                $admin_data = mysqli_fetch_assoc($admin_info);
+                session_start();
+                $_SESSION['adminID']= $admin_data['id'];
+                $_SESSION['admin_name']= $admin_data['admin_name'];
+
+                }
+            }
+        }
+
+        public function adminLogout(){
+            unset($_SESSION['adminID']);
+            unset($_SESSION['admin_name']);
+            header('location: index.php');
+
+        }
+
+
     }
-}
 
 
+       
 
 
 
